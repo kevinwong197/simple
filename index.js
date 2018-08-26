49dde37787e2916146be90150a584312c62b58a0
kevinwong197 <kevinwong1997@hotmail.com>
2018-08-26 12:22:35 -0400

    fs.createWriteStream(path).on('finish' ...

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const fs = require('fs');
 const tmp = require('tmp');
 
-const {app, BrowserWindow, Menu, Notification, ipcMain} = require('electron');
+const {app, BrowserWindow, Menu, Notification, ipcMain, nativeImage} = require('electron');
 const path = require('path');
 const url = require('url');
 
@@ -28,51 +28,55 @@ function createWindow(urlstr) {
 }
 
 function mogrify(path, callback) {
-  let mogrify = require('child_process').spawn('resources/mogrify.exe',
-    [path, '-define icon:auto-resize=64,48,32,16'])
-  mogrify.on('exit', callback);
+  require('child_process')
+    .spawn('resources/mogrify.exe', [path, '-define icon:auto-resize=64,48,32,16'])
+    .on('exit', callback);
 }
 
-app.on('ready', function() {
+function setWinIcon(win, path) {
+  if(win) {
+    win.setIcon(nativeImage.createFromPath(path));
+  }
+}
+
+app.on('ready', () => {
   createWindow("https://google.com");
 })
 
-app.on('web-contents-created', function (webContentsCreatedEvent, contents) {
+app.on('web-contents-created', (webContentsCreatedEvent, contents) => {
   if (contents.getType() === 'webview') {
-    contents.on('new-window', function (event, urlstr) {
+    contents.on('new-window', (event, urlstr) => {
       let owner = event.sender.getOwnerBrowserWindow();
       let win = createWindow(urlstr);
       event.preventDefault();
     });
 
-    contents.on('page-favicon-updated', function (event, faviconUrls) {
+    contents.on('page-favicon-updated', (event, faviconUrls) => {
       let owner = event.sender.getOwnerBrowserWindow();
       if (faviconUrls.length >= 1) {
         axios({'url': faviconUrls[faviconUrls.length-1], 'responseType': 'stream'})
         .then(response => {
-          tmp.file({prefix: 'simple-', postfix: '.ico'}, (err, path, fd, cleanupCallback) => {
-            if (err) throw err;
-            response.data.pipe(fs.createWriteStream(path));
-            mogrify(path, () => {
-              if(owner) {
-                owner.setIcon(path);
-              }
-            })
-          });
+          tmp.file(
+            {prefix: 'simple-', postfix: '.ico'},
+            (err, path, fd, cleanupCallback) => {
+              if (err) throw err;
+              response.data.pipe(fs.createWriteStream(path).on('finish', () => {
+                mogrify(path, () => setWinIcon(owner, path));
+              }));
+            }
+          );
         }).catch(err => {
           console.log(err);
-          if(owner) {
-            owner.setIcon('resources/default.ico');
-          }
+          setWinIcon(owner, 'resources/default.ico');
         });
       } else {
-        owner.setIcon('resources/default.ico');
+        setWinIcon(owner, 'resources/default.ico');
       }
     });
   }
 });
 
-app.on('browser-window-created', function (e, win) {
+app.on('browser-window-created', (e, win) => {
   win.setMenu(null);
   // win.webContents.toggleDevTools();
   win.webContents.on('before-input-event', (event, input) => {
@@ -86,6 +90,6 @@ app.on('browser-window-created', function (e, win) {
   })
 })
 
-app.on('window-all-closed', function () {
+app.on('window-all-closed', () => {
   app.quit();
 })
